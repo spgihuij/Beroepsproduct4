9469685789d06e15709b1df6c098514073526698
Jeremy <jeremy.bovenkamp@gmail.com>
2019-05-06 14:41:31 +0200

    6-5-2019 14:41

--- app/src/main/java/com/example/beroepsproduct4/OverzichtData.java
+++ app/src/main/java/com/example/beroepsproduct4/OverzichtData.java
@@ -1,13 +1,130 @@
 package com.example.beroepsproduct4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+/**
+ * Created by User on 2/8/2017.
+ */
 
 public class OverzichtData extends AppCompatActivity {
+    private static final String TAG = "ViewDatabase";
+
+    //add Firebase Database stuff
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private  String userID;
+
+    private ListView mListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overzicht_data);
+
+        mListView = (ListView) findViewById(R.id.listview);
+
+        //declare the database reference object. This is what we use to access the database.
+        //NOTE: Unless you are signed in, this will not be useable.
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void showData(DataSnapshot dataSnapshot) {
+        for(DataSnapshot ds : dataSnapshot.getChildren()){
+            UserInformation uInfo = new UserInformation();
+            uInfo.setVoorNaam(ds.child(userID).getValue(UserInformation.class).getVoorNaam()); //set the name
+            uInfo.setAchterNaam(ds.child(userID).getValue(UserInformation.class).getAchterNaam()); //set the email
+            uInfo.setEmailAdres(ds.child(userID).getValue(UserInformation.class).getEmailAdres()); //set the phone_num
+
+            //display all the information
+            Log.d(TAG, "showData: naam: " + uInfo.getVoorNaam());
+            Log.d(TAG, "showData: voornaam: " + uInfo.getAchterNaam());
+            Log.d(TAG, "showData: emailadres: " + uInfo.getEmailAdres());
+
+            ArrayList<String> array  = new ArrayList<>();
+            array.add(uInfo.getVoorNaam());
+            array.add(uInfo.getAchterNaam());
+            array.add(uInfo.getEmailAdres());
+            ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,array);
+            mListView.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/beroepsproduct4/UserInformation.java
@@ -0,0 +1,33 @@
+package com.example.beroepsproduct4;
+
+public class UserInformation {
+
+    private String VoorNaam;
+
+    public String getVoorNaam() {
+        return VoorNaam;
+    }
+
+    public void setVoorNaam(String voorNaam) {
+        VoorNaam = voorNaam;
+    }
+
+    public String getAchterNaam() {
+        return AchterNaam;
+    }
+
+    public void setAchterNaam(String achterNaam) {
+        AchterNaam = achterNaam;
+    }
+
+    public String getEmailAdres() {
+        return EmailAdres;
+    }
+
+    public void setEmailAdres(String emailAdres) {
+        EmailAdres = emailAdres;
+    }
+
+    private String AchterNaam;
+    private String EmailAdres;
+}
