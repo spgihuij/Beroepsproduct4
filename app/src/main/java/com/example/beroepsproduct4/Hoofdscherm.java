b6d3ff025519542d87bdfc47df4bcae9de73d9e3
Rensvanherwaarden <42996111+Rensvanherwaarden@users.noreply.github.com>
2019-05-07 12:49:23 +0200

    aanpassen hoofdscherm + kleuren van de applicatie geupdate

--- app/src/main/java/com/example/beroepsproduct4/Hoofdscherm.java
+++ app/src/main/java/com/example/beroepsproduct4/Hoofdscherm.java
@@ -1,12 +1,132 @@
 /**
+ * package com.example.beroepsproduct4;
+ * <p>
+ * import android.content.Intent;
+ * import android.os.Bundle;
+ * import android.support.annotation.NonNull;
+ * import android.support.design.widget.FloatingActionButton;
+ * import android.support.design.widget.Snackbar;
+ * import android.view.View;
+ * import android.support.design.widget.NavigationView;
+ * import android.support.v4.view.GravityCompat;
+ * import android.support.v4.widget.DrawerLayout;
+ * import android.support.v7.app.ActionBarDrawerToggle;
+ * import android.support.v7.app.AppCompatActivity;
+ * import android.support.v7.widget.Toolbar;
+ * import android.view.Menu;
+ * import android.view.MenuItem;
+ * import android.widget.Button;
+ * import android.widget.TextView;
+ * <p>
+ * import com.example.beroepsproduct4.R;
+ * import com.google.firebase.auth.FirebaseAuth;
+ * import com.google.firebase.auth.FirebaseUser;
+ * <p>
+ * public class Hoofdscherm extends AppCompatActivity
+ * implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+ * //drawer
+ * <p>
+ * //Servi Werkend
+ * private FirebaseAuth firebaseAuth;
+ * private TextView textViewGebruikeremail, textViewGebruikeremailNAV;
+ * private Button btnUitlog2;
+ * private DrawerLayout drawer;
+ *
+ * @Override protected void onCreate(Bundle savedInstanceState) {
+ * super.onCreate(savedInstanceState);
+ * setContentView(R.layout.activity_main);
+ * Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+ * setSupportActionBar(toolbar);
+ * <p>
+ * <p>
+ * DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+ * ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+ * this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+ * drawer.addDrawerListener(toggle);
+ * toggle.syncState();
+ * <p>
+ * NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+ * navigationView.setNavigationItemSelectedListener(this);
+ * <p>
+ * //Servi
+ * firebaseAuth = FirebaseAuth.getInstance();
+ * if (firebaseAuth.getCurrentUser() == null) {
+ * finish();
+ * startActivity(new Intent(this, Inlogscherm.class));
+ * }
+ * FirebaseUser user = firebaseAuth.getCurrentUser();
+ * /**
+ * textViewGebruikeremail = (TextView) findViewById(R.id.textViewGebruikeremail);
+ * textViewGebruikeremail.setText("Welkom " + user.getEmail());
+ * <p>
+ * btnUitlog2 = (Button) findViewById(R.id.btnUitlog2);
+ * btnUitlog2.setOnClickListener(this);
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * }
+ * @Override public void onBackPressed() {
+ * DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+ * if (drawer.isDrawerOpen(GravityCompat.START)) {
+ * drawer.closeDrawer(GravityCompat.START);
+ * } else {
+ * super.onBackPressed();
+ * }
+ * }
+ * @Override public boolean onCreateOptionsMenu(Menu menu) {
+ * // Inflate the menu; this adds items to the action bar if it is present.
+ * getMenuInflater().inflate(R.menu.main, menu);
+ * return true;
+ * }
+ * @Override public boolean onOptionsItemSelected(MenuItem item) {
+ * // Handle action bar item clicks here. The action bar will
+ * // automatically handle clicks on the Home/Up button, so long
+ * // as you specify a parent activity in AndroidManifest.xml.
+ * int id = item.getItemId();
+ * <p>
+ * //noinspection SimplifiableIfStatement
+ * if (id == R.id.action_settings) {
+ * return true;
+ * }
+ * <p>
+ * return super.onOptionsItemSelected(item);
+ * }
+ * @Override public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+ * switch (menuItem.getItemId()) {
+ * <p>
+ * // voor menu items die geselecteerd blijven als je er op klikt
+ * <p>
+ * case R.id.nav_camera:
+ * getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EvenementAanmaken()).commit();
+ * break;
+ * <p>
+ * //  case R.id.nav_gallery:
+ * //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EvenementLijst()).commit();
+ * //  break;
+ * <p>
+ * }
+ * DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+ * <p>
+ * drawer.closeDrawer(GravityCompat.START);
+ * return true;
+ * }
+ * @Override public void onClick(View view) {
+ * if (view == btnUitlog2) {
+ * firebaseAuth.signOut();
+ * finish();
+ * startActivity(new Intent(this, Inlogscherm.class));
+ * }
+ * }
+ * }
+ */
 
 package com.example.beroepsproduct4;
+
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,166 +137,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.beroepsproduct4.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class Hoofdscherm extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
-    //drawer
-
-    //Servi Werkend
-    private FirebaseAuth firebaseAuth;
-    private TextView textViewGebruikeremail, textViewGebruikeremailNAV;
-    private Button btnUitlog2;
-    private DrawerLayout drawer;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-//Servi
-        firebaseAuth = FirebaseAuth.getInstance();
-        if (firebaseAuth.getCurrentUser() == null) {
-            finish();
-            startActivity(new Intent(this, Inlogscherm.class));
-        }
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        /**
-        textViewGebruikeremail = (TextView) findViewById(R.id.textViewGebruikeremail);
-        textViewGebruikeremail.setText("Welkom " + user.getEmail());
-
-        btnUitlog2 = (Button) findViewById(R.id.btnUitlog2);
-        btnUitlog2.setOnClickListener(this);
-
-
-
-
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-
-            // voor menu items die geselecteerd blijven als je er op klikt
-
-            case R.id.nav_camera:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EvenementAanmaken()).commit();
-                break;
-
-          //  case R.id.nav_gallery:
-            //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EvenementLijst()).commit();
-              //  break;
-
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        if (view == btnUitlog2) {
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(this, Inlogscherm.class));
-        }
-    }
-}
- *
- */
-
-package com.example.beroepsproduct4;
-
-
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Hoofdscherm extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth firebaseAuth;
-
     private ArrayList<String> ontwikkelaars = new ArrayList<String>();
-
     private static final String TAG = "MyActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -197,53 +181,38 @@ public class Hoofdscherm extends AppCompatActivity
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null)
-                {
+                if (user != null) {
                     checkUser(navigationView);
                 }
             }
         };
         firebaseAuth.addAuthStateListener(authStateListener);
 
+        // random zinnen
+        final TextView textone=(TextView) findViewById(R.id.begroetingszinnen_hoofdscherm);
+        Button generate= (Button)findViewById(R.id.btnzinnengenerate);
+        final String [] voelgoedzinnen={"voel je je goed", "hoe voel je je vandaag", "helemaal top vandaag"};
 
+        int rando= (int)((Math.random()*3));
+
+        generate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int rando= (int)(Math.random()*3);
+                textone.setText(voelgoedzinnen[rando]);
+            }
+        });
     }
 
-    /* public void createUser()
-     {
- test
-         firebaseAuth.createUserWithEmailAndPassword("thomasvanderlubbe2@hotmail.com", "123456")
-                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                     @Override
-                     public void onComplete(@NonNull Task<AuthResult> task) {
-                         if (task.isSuccessful()) {
-                             FirebaseUser user = firebaseAuth.getCurrentUser();
-                             ontwikkelaars.add(user.getUid());
-                         } else {
-                             // If sign in fails, display a message to the user.
-                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                             Toast.makeText(Hoofdscherm.this, "Authentication failed.",
-                                     Toast.LENGTH_SHORT).show();
-                         }
-                     }
-                 });
-     }*/
-
-
-    public void checkUser(NavigationView navigationView)
-    {
-        for(String x : ontwikkelaars)
-        {
+    public void checkUser(NavigationView navigationView) {
+        for (String x : ontwikkelaars) {
             FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
-            if (user.getUid().equals(x))
-            {
+            if (user.getUid().equals(x)) {
                 Menu nav_Menu = navigationView.getMenu();
                 nav_Menu.findItem(R.id.evenement_toevoegen).setVisible(true);
                 nav_Menu.findItem(R.id.sociaal_netwerk_toevoegen).setVisible(true);
                 nav_Menu.findItem(R.id.geen_ontwikkelaar).setVisible(false);
-            }
-
-            else
-            {
+            } else {
                 Menu nav_Menu = navigationView.getMenu();
                 nav_Menu.findItem(R.id.evenement_toevoegen).setVisible(false);
                 nav_Menu.findItem(R.id.sociaal_netwerk_toevoegen).setVisible(false);
@@ -252,6 +221,7 @@ public class Hoofdscherm extends AppCompatActivity
             }
         }
     }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -277,7 +247,10 @@ public class Hoofdscherm extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, Inlogscherm.class));
             return true;
         }
 
---
+++ app/src/main/java/com/example/beroepsproduct4/Navigation_activity.java
@@ -0,0 +1,4 @@
+package com.example.beroepsproduct4;
+
+public class Navigation_activity {
+}
